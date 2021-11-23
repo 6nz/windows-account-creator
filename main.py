9e32f9064a85d0aced72f691eba46f82a2395c5e
SKYNETMARCI <74118308+KAMKAZEMARCI@users.noreply.github.com>
2021-11-23 19:11:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-import string, random, threading, os, sys, subprocess
+import string, random, threading, time, os, sys, subprocess
 from colorama import Fore, Back, Style
 
+time.sleep(0.5)
 count = input("Thread count: ")
+accname = input("Account name: ")
 
-def createacc():
+def createaccsloop():
     try:
         nums = string.digits
         new = ''.join(random.choice(nums) for i in range(5))
-        os.system(f'title {new}')
-        print(Fore.GREEN + f"Adding new user: {new}")
-        subprocess.os.system(f'net user Libes{new} Libes{new} /add')
-        print(Fore.GREEN + f"Successfully added new user! User: {new}")
+        print(Fore.GREEN + f"Adding new user: {accname}{new}")
+        subprocess.os.system(f'net user {accname}{new} {accname}{new} /add')
+        print(Fore.GREEN + f"Successfully added new user! User: {accname}{new}")
     except:
         print(Fore.RED + 'Failed!')
         pass
 
 def thread():
+    acccount = 0
+    threadcount = 0
     for i in range(int(count)):
-        threading.Thread(target=createacc).start()
+        threading.Thread(target=createaccsloop).start()
+        threadcount += 1
+        acccount += 1
+        os.system(f'title Total created accounts: {acccount} ┃ Thread count: {count}')
 
 
 def starter():
+    time.sleep(0.5)
     print(Fore.RED + 'Starting program!')
+    time.sleep(0.5)
     thread()
 
 start = input("Press anything to start the program!")
 os.system('cls')
 
+time.sleep(0.5)
 starter()
+
+time.sleep(5)
